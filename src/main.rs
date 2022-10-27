ff4e33355fd3f04f022bca16293b44b50b31e39f
JadKHaddad <jadkhaddad@gmail.com>
2022-10-27 08:07:13 +0200

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -3,99 +3,54 @@ Will parse every Lambda-calculus term as long as it is (right) parenthesized.
 Not-Parenthesized terms are only allowed as an abstraction with the Dot operator. e.g. [λx . x], [λx . (λy . (x y))]
 Summery: Not-Parenthesized terms and Dot operator are not well supported.
 */
-
+use serde::{Deserialize, Serialize};
+use std::fs;
 pub mod ast;
 pub mod term;
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct InputTerm {
+    term: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    print_ast: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    evaluate: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    reduce: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    reduce_steps: Option<u32>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Input {
+    terms: Vec<InputTerm>,
+}
+
 fn main() {
-    //Dot operator //Note: "." can only be placed with a blank space before it.
-    println!("----------------------------");
-    let term = term::SParser::new().parse("(λz . (xx))").unwrap();
-    println!("Term: [ {} ]", term);
-    println!("----------------------------");
-    let term = term::SParser::new()
-        .parse("((λx . (xx))(λx . (xx)))")
-        .unwrap();
-    println!("Term: [ {} ]", term);
-    println!("----------------------------");
-    //No Outer Parenthesis with Dot Operator
-    let term = term::SParser::new().parse("λz . (xx)").unwrap();
-    println!("Term: [ {} ]", term);
-    println!("----------------------------");
-    let term = term::SParser::new().parse("λxyzu . (xx)").unwrap();
-    println!("Term: [ {} ]", term);
-    println!("----------------------------");
-    //End of No Outer Parenthesis with Dot Operator
-    //End of Dot operator
-    let term = term::SParser::new().parse("(λa (λb (λc (x x))))").unwrap();
-    println!("Term: [ {} ]", term);
-    println!("----------------------------");
-    let term = term::SParser::new()
-        .parse("((λx (x * ((λx (x + ((λx x) 3))) 8))) 3)")
-        .unwrap();
-    println!("Term: [ {} ] = [ {} ]", term, term.evaluate());
-    println!("----------------------------");
-    let term = term::SParser::new()
-        .parse("((λx ((λy (x * y)) 4)) 3)")
-        .unwrap();
-    println!("Term: [ {} ] = [ {} ]", term, term.evaluate());
-    println!("Term: [ {:?} ]", term);
-    println!("----------------------------");
-    let term = term::SParser::new()
-        .parse("((λx ((λy (x + y)) 8)) 100)")
-        .unwrap();
-    println!("Term: [ {} ] = [ {} ]", term, term.evaluate());
-    println!("----------------------------");
-    let mut term = term::SParser::new()
-        .parse("(((λx (λx (f x))) x) ((λx x) z))")
-        .unwrap();
-    let term_temp = term.clone();
-    println!("Term: [ {} ]", term_temp);
-    term.reduce();
-    println!("Term: [ {} ] (After reduction) = [ {} ]", term_temp, term);
-    term.reduce();
-    println!(
-        "Term: [ {} ] (After deep reduction) = [ {} ]",
-        term_temp, term
-    );
-    println!("----------------------------");
-    let mut term = term::SParser::new()
-        .parse("((((λx (x y)) (λz (x z))) f) (g h))")
-        .unwrap();
-    let term_temp = term.clone();
-    println!("Term: [ {} ]", term_temp);
-    term.reduce();
-    println!("Term: [ {} ] (After reduction) = [ {} ]", term_temp, term);
-    term.reduce();
-    println!(
-        "Term: [ {} ] (After deep reduction) = [ {} ]",
-        term_temp, term
-    );
-    println!("----------------------------");
-    let term = term::SParser::new()
-        .parse("((λx((λy((x+y)*y))3))4)")
-        .unwrap();
-    println!("Term: [ {} ] = [ {} ]", term, term.evaluate());
-    println!("Term: [ {:?} ]", term);
-    println!("----------------------------");
-    let term = term::SParser::new().parse("(((ux)(yz))(λv(vy)))").unwrap();
-    println!("Term: [ {} ]", term);
-    println!("Term: [ {:?} ]", term);
-    println!("----------------------------");
-    let term = term::SParser::new()
-        .parse("((((λx(λy(λz((xz)(yz)))))u)v)w)")
-        .unwrap();
-    println!("Term: [ {} ]", term);
-    println!("Term: [ {:?} ]", term);
-    println!("----------------------------");
-    let term = term::SParser::new()
-        .parse("(((w(λx(λy(λz((xz)(yz))))))u)v)")
-        .unwrap();
-    println!("Term: [ {} ]", term);
-    println!("Term: [ {:?} ]", term);
-    println!("----------------------------");
-    let term = term::SParser::new().parse("((λx(xx))(λx(xx)))").unwrap();
-    println!("Term: [ {} ]", term);
-    println!("Term: [ {:?} ]", term);
+    let input = fs::read_to_string("terms.yaml").expect("Unable to read file");
+    let input: Input = serde_yaml::from_str(&input).expect("Unable to parse YAML");
+    for input_term in input.terms {
+        println!("----------------------------");
+        let mut term = term::SParser::new().parse(&input_term.term).unwrap();
+        println!("Term: [ {} ]", term);
+
+        if input_term.print_ast.unwrap_or(false) {
+            println!("AST: {:?}", term);
+        }
+        if input_term.evaluate.unwrap_or(false) {
+            println!("Evaluated: [ {} ]", term.evaluate());
+        }
+        if input_term.reduce.unwrap_or(false) {
+            if let Some(reduce_steps) = input_term.reduce_steps {
+                for _ in 0..reduce_steps {
+                    term.reduce();
+                    println!("Reduced: [ {} ]", term);
+                }
+            } else {
+                term.reduce();
+                println!("Reduced: [ {} ]", term);
+            }
+        }
+    }
     println!("----------------------------");
 }
